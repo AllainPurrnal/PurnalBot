b1d4beb05a3da99690a9975a369b19f956a1c109
AllainPurrnal <llnbrnl017@gmail.com>
2020-06-03 22:41:10 -0700

    Added pulling random gods from specific Pantheons or Classes

--- commands/help.js
+++ commands/help.js
@@ -1,3 +1,4 @@
+// const Discord = require('discord.js');
 const { prefix } = require('../config.json');
 
 module.exports = {
@@ -7,18 +8,30 @@ module.exports = {
 	usage: '[command name]',
 	cooldown: 5,
 	execute(message, args) {
-    const data = [];
+    // const data = [];
     const { commands } = message.client;
+    const helpCard = {
+      color: `#FF00B4`,
+      title: 'The Litter Box',
+      thumbnail: {
+        url: 'https://i.imgur.com/SbvmSmR.jpg'
+      }
+    }
 
+    // Send list of commands in a PM
     if(!args.length) {
-      data.push('Here\'s a list of all my commands:');
-      data.push(commands.map(command => command.name).join(', '));
-      data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
+      helpCard.description = 'Here\'s a list of all my commands:';
+      helpCard.footer = {text: `\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`}
 
-      return message.author.send(data, { split: true })
+      helpCard.fields = commands.map(command => {
+        return {name: `${prefix}${command.name}`, value: `${command.description}`, inline: true}
+      })
+
+
+      return message.author.send({embed: helpCard})
       	.then(() => {
       		if (message.channel.type === 'dm') return;
-      		message.reply('I\'ve sent you a DM with all my commands!');
+      		message.reply('I slid into your DM\'s with all my commands!');
       	})
       	.catch(error => {
       		console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
@@ -26,21 +39,20 @@ module.exports = {
       	});
     }
 
+    // Sends more info about a specific command
     const name = args[0].toLowerCase();
     const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
-      
+
     if (!command) {
     	return message.reply('that\'s not a valid command!');
     }
+
+    helpCard.title = `${prefix}${command.name}`
+    helpCard.description = `${command.description}`
+    if (command.cooldown) helpCard.fields = {name: 'Cooldown', value: `${command.cooldown}`}
+    if (command.usage) helpCard.fields = {name: 'Usage:', value: `${prefix}${command.name} ${command.usage}`}
+
+    message.channel.send({embed: helpCard})
     
-    data.push(`**Name:** ${command.name}`);
-    
-    if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
-    if (command.description) data.push(`**Description:** ${command.description}`);
-    if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
-    
-    data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
-    
-    message.channel.send(data, { split: true });
 	},
 };
--- commands/rgod.js
+++ commands/rgod.js
@@ -1,50 +1,134 @@
 const Discord = require('discord.js');
 const { pantheon } = require('../smite.json');
 
+let godList = [];
+
+const buildCard = (message, godList) => {
+  let num = Math.floor(Math.random() * (godList.length))
+  let name = godList[num].name;
+  let title = godList[num].title;
+  let icon = godList[num].icon;
+  let pan = godList[num].pantheon
+  let cClass = godList[num].class
+
+  const color = () => {
+    switch (cClass) {
+      case 'Warrior':
+        return `#de0000`
+      case 'Mage':
+        return `#b200de`
+      case 'Hunter':
+        return `#ff8c00`
+      case 'Guardian':
+        return `#44cc00`
+      case 'Assassin':
+        return `#ffff00`
+      default: return `#FF00B4`
+    } 
+  }
+
+  const godCard = new Discord.MessageEmbed()
+    .setColor(color())
+    .setTitle(`${name}`)
+    .setDescription(`${title}`)
+    .setThumbnail(`${icon}`)
+    .addField('Pantheon', `${pan}`)
+
+  return message.reply(godCard)
+};
+
+const randomGod = (message) => {
+  godList = [];
+
+  pantheon.map(p => {
+    p.gods.map(god => {
+      return godList.push(god)
+    })
+  })
+  buildCard(message, godList)
+};
+
+const randomGodPantheon = (message, choice) => {
+  godList = [];
+
+  if (choice == 'great') {
+    pantheon[4].gods.map(god => {
+      // console.log(god.name)
+      return godList.push(god)
+    })
+
+    // console.log(godList[0])
+    return buildCard(message, godList)
+  }
+
+  let isChoice = ((p) => {
+    return p.name.toLowerCase() === choice
+  });
+
+  let pan = pantheon.filter(isChoice)
+
+  pan[0].gods.map((g) => {
+    return godList.push(g)
+  })
+
+  // console.log(godList)
+  buildCard(message, godList)
+};
+
+const randomGodClass = (message, choice) => {
+  godList = [];
+
+  pantheon.map(p => {
+    p.gods.map(god => {
+      return godList.push(god)
+    })
+  })
+
+  let isChoice = ((p) => {
+    return p.class.toLowerCase() === choice
+  });
+
+  godList = godList.filter(isChoice)
+
+  buildCard(message, godList)
+};
+
 module.exports = {
   name: 'rgod',
-  description: 'Chooses a random god for SMITE',
+  description: 'Chooses a random god for SMITE. Specify a Pantheon or Class for a narrowed random God.',
+	usage: '<pantheon> || <class>',
   execute(message, args) {
-    let godList = [];
-    let name, title, icon, pan;
+    let choice;
+    if (!args.length) choice = args
+    else choice = args[0].toLowerCase();
 
-    pantheon.map(p => {
-      p.gods.map(god => {
-        return godList.push(god)
-      })
-    })
-  
-    let num = Math.floor(Math.random() * (godList.length - 1)) + 1
-    name = godList[num].name;
-    title = godList[num].title;
-    icon = godList[num].icon;
-    pan = godList[num].pantheon
-    charClass = godList[num].class
+    switch (choice) {
+      case "arthurian":
+      case "celtic":
+      case "chinese":
+      case "egyptian":
+      case "great":
+      case "greek":
+      case "hinder":
+      case "japanese":
+      case "mayan":
+      case "norse":
+      case "polynesian":
+      case "roman":
+      case "slavic":
+      case "voodoo":
+      case "yoruba":
+        return randomGodPantheon(message, choice)
 
-    const color = () => {
-      switch (charClass) {
-        case 'Warrior':
-          return `#de0000`
-        case 'Mage':
-          return `#b200de`
-        case 'Hunter':
-          return `#ff8c00`
-        case 'Guardian':
-          return `#44cc00`
-        case 'Assassin':
-          return `#ffff00`
-        default: return `#FF00B4`
-      } 
+      case "hunter":
+      case "warrior":
+      case "mage":
+      case "guardian":
+      case "assassin":
+        return randomGodClass(message, choice)
+
+      default: return randomGod(message)
     }
-
-    const godCard = new Discord.MessageEmbed()
-      .setColor(color())
-      .setTitle(`${name}`)
-      .setDescription(`${title}`)
-      .setThumbnail(`${icon}`)
-      .addField('Pantheon', `${pan}`)
-  
-    return message.reply(godCard)
 
   }
 }
