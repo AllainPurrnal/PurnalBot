3b14c63fc9179d1e06667a0c4235bd516692b668
AllainPurrnal <llnbrnl017@gmail.com>
2020-06-03 23:45:43 -0700

    Changed config to .env

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
+require('dotenv').config()
+
+let prefix = process.env.PREFIX;
+let token = process.env.TOKEN;
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
