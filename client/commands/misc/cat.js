dfadbe53cc673a86ebc35cfae7191f25d46d900c
AllainPurrnal <llnbrnl017@gmail.com>
2020-06-17 23:19:21 -0700

    Starting a test deploy on heroku with express api backend

---
+++ client/commands/misc/cat.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+
+const cat = `https://aws.random.cat/meow`
+
+module.exports = {
+  name: 'cat',
+  description: `Get a random cat!`,
+  execute(message) {
+    axios.get(`${cat}`)
+      .then(res => {
+        message.channel.send(res.data.file)
+      })
+      .catch(err => {
+        message.reply(`Woops, couldn't find a random cat 😿`)
+      })
+  }
+}
